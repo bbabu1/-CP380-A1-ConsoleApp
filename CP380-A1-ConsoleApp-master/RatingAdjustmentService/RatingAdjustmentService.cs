8d861f1ab9a62f922efbaf5bab259a7f13714e74
BONY BABU <65872018+bbabu1@users.noreply.github.com>
2021-06-23 18:27:03 -0400

    edited cs

--- CP380-A1-ConsoleApp-master/RatingAdjustmentService/RatingAdjustmentService.cs
+++ CP380-A1-ConsoleApp-master/RatingAdjustmentService/RatingAdjustmentService.cs
@@ -1,45 +1,48 @@
-﻿using System;
+using System;
 
 namespace RatingAdjustment.Services
 {
-    /** Service calculating a star rating accounting for the number of reviews
-     * 
-     */
+
     public class RatingAdjustmentService
     {
-        const double MAX_STARS = 5.0;  // Likert scale
-        const double Z = 1.96; // 95% confidence interval
+        const double MAX_STARS = 5.0;
+        const double Z = 1.96;
 
         double _q;
         double _percent_positive;
 
-        /** Percentage of positive reviews
-         * 
-         * In this case, that means X of 5 ==> percent positive
-         * 
-         * Returns: [0, 1]
-         */
         void SetPercentPositive(double stars)
         {
-            // TODO: Implement this!
+            _percent_positive = (stars * 20.0) / 100.0;
         }
 
-        /**
-         * Calculate "Q" given the formula in the problem statement
-         */
         void SetQ(double number_of_ratings)
         {
-         // TODO: Implement this!
+            double a = _percent_positive;
+            double b = number_of_ratings;
+            _q = Z * Math.Sqrt(((a * (1.0 - a)) + ((Z * Z) / (40 * b))) / b);
         }
 
-        /** Adjusted lower bound
-         * 
-         * Lower bound of the confidence interval around the star rating.
-         * 
-         * Returns: a double, up to 5
-         */
-        public double Adjust(double stars, double number_of_ratings) {
-            // TODO: Implement this!
+        public double Adjust(double stars, double number_of_ratings)
+        {
+            if (stars <= MAX_STARS)
+            {
+                SetPercentPositive(stars);
+                SetQ(number_of_ratings);
+                double a = _percent_positive;
+                double b = number_of_ratings;
+                double c = _q;
+                double lb = ((a + ((Z * Z) / (2.0 * b)) - c) / (1.0 + ((Z * Z) / b)));
+                double lbnd = (lb / 20.0) * 100.0;
+                if (lbnd <= MAX_STARS)
+                {
+                    return lbnd;
+                }
+                else
+                {
+                    return stars;
+                }
+            }
             return 0.0;
         }
     }
